8078f1662bfa6e744e80770a47f44cead66d8583
osm3rr <osm3rr@adakademy.com>
2025-02-27 17:31:36 -0400

    model Publication added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Publication
 
 # Register your models here.
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date', 'updated')
+    list_filter = ('author', 'date', 'updated')
+    search_fields = ('title', 'body', 'author__username')
+    ordering = ('-date',)
+
+
+admin.site.register(Publication, PublicationAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Publication(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    
