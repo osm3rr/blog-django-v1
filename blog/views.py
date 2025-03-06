18edfa3dad5fdf07646439e5496464d522754b44
osm3rr <osm3rr@adakademy.com>
2025-03-06 17:50:15 -0400

    publications list done

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Publication
+from django.views.generic import ListView
 
 # Create your views here.
+
+class PublicationListView(ListView):
+    model = Publication
+    template_name = 'publications-list.html'
