af1a04ef46305e8a210a6c65dc988905f88f9f60
osm3rr <osm3rr@adakademy.com>
2025-03-13 17:11:46 -0400

    create done

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Publication(models.Model):
@@ -13,5 +14,8 @@ class Publication(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('publication-detail', kwargs={'pk': self.pk})
 
     
