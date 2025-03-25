d86215355043c7645f0f15a7af1ac6fd13a42c7b
osm3rr <osm3rr@adakademy.com>
2025-03-25 17:38:29 -0400

    loginrequiedmixin added

--- blog/views.py
+++ blog/views.py
@@ -2,27 +2,28 @@ from .models import Publication
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class PublicationListView(ListView):
+class PublicationListView(LoginRequiredMixin, ListView):
     model = Publication
     template_name = 'publications-list.html'
 
-class PublicationDetailView(DetailView):
+class PublicationDetailView(LoginRequiredMixin, DetailView):
     model = Publication
     template_name = 'publication-detail.html'
 
-class PublicationCreateView(CreateView):
+class PublicationCreateView(LoginRequiredMixin, CreateView):
     model = Publication
     template_name = 'publication-create.html'
     fields = ['title', 'body', 'author']
 
-class PublicationUpdateView(UpdateView):
+class PublicationUpdateView(LoginRequiredMixin, UpdateView):
     model = Publication
     template_name = 'publication-update.html'
     fields = ['title', 'body']
 
-class PublicationDeleteView(DeleteView):
+class PublicationDeleteView(LoginRequiredMixin, DeleteView):
     model = Publication
     template_name = 'publication-delete.html'
     success_url = reverse_lazy('publications-list')
